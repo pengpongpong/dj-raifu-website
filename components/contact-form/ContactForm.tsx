76076fc122c72977eb04e8954e6f0ed8df6f307e
Phuong T <t.m.phuong@hotmail.com>
2023-07-25 00:38:10 +0200

    added alt to social image links

--- components/contact-form/ContactForm.tsx
+++ components/contact-form/ContactForm.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { ChangeEvent, ChangeEventHandler, EventHandler, FormEvent, useEffect, useRef, useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import { useForm } from "react-hook-form"
 import { yupResolver } from "@hookform/resolvers/yup"
 import * as yup from "yup"
@@ -11,13 +11,20 @@ const ErrorText = ({ text }: { text?: string }) => {
 
 // location, date, time, message
 const ContactForm = () => {
+    const [selectedDate, setSelectedDate] = useState<Date>();
+    const [selectOption, setSelectOption] = useState<string>("")
+
     const errorMessage = {
         name: "Bitte Name eintragen",
-        email: "Bitte Email eintragen"
+        email: "Bitte Email eintragen",
+        call: "Bitte Telefonnummer eintragen",
+        type: "Bitte Kontaktart angeben"
     }
     const schema = yup.object({
         name: yup.string().required(errorMessage.name),
-        email: yup.string().email().required(errorMessage.email),
+        type: yup.string(),
+        email: yup.string().email(),
+        call: yup.string(),
     })
 
     const {
@@ -30,17 +37,17 @@ const ContactForm = () => {
         defaultValues: {
             name: "",
             email: "",
+            call: "",
+            type: ""
         }
     })
 
-
     const onSubmit = handleSubmit((data) => {
         console.log(data)
-        console.log(selected instanceof Date)
+        console.log(selectedDate instanceof Date)
+        console.log(!!selectedDate)
+        console.log(selectOption)
     })
-
-    const [selected, setSelected] = useState<Date>();
-    const [selectOption, setSelectOption] = useState<string>("")
 
     const handleSelect = (event: ChangeEvent<HTMLSelectElement>) => {
         const { target } = event
@@ -52,21 +59,25 @@ const ContactForm = () => {
             <fieldset className="font-text flex flex-col gap-4">
                 <input type="text" placeholder="Dein Name" className="daisy_input daisy_input-bordered daisy_input-primary w-full border border-white text-white" {...register("name")} />
                 {errors?.name && <ErrorText text={errors?.name?.message} />}
-                <select className="daisy_select daisy_select-bordered daisy_select-primary w-full" onChange={handleSelect}>
+
+                <select name="type" className="daisy_select daisy_select-bordered daisy_select-primary w-full" onChange={handleSelect} >
                     <option disabled selected>Wie möchtest du kontaktiert werden?</option>
                     <option value="email">Email</option>
                     <option value="call">Anruf</option>
                 </select>
+                {/* {!selectOption && <ErrorText text={errorMessage.type} />} */}
+
                 {selectOption === "email" ? <input type="email" placeholder="deine@email.com" className="daisy_input daisy_input-bordered daisy_input-primary w-full border border-white text-white" {...register("email")} /> : ""}
-                {errors?.email && <ErrorText text={errors?.email?.message} />}
-                {selectOption === "call" ? <input type="text" placeholder="Deine Telefonnummer" className="daisy_input daisy_input-bordered daisy_input-primary w-full border border-white text-white" {...register("email")} /> : ""}
+                {errors?.email && selectOption && <ErrorText text={errors?.email?.message} />}
+
+                {selectOption === "call" ? <input type="text" placeholder="Deine Telefonnummer" className="daisy_input daisy_input-bordered daisy_input-primary w-full border border-white text-white" {...register("call")} /> : ""}
+                {errors?.call && selectOption && <ErrorText text={errors?.call?.message} />}
                 <DatePickerForm
-                    selected={selected}
-                    setSelected={setSelected}
+                    selected={selectedDate}
+                    setSelected={setSelectedDate}
                 />
                 <textarea className="daisy_textarea daisy_textarea-bordered daisy_textarea-primary" rows={4} placeholder="Hinterlasse mir eine Nachricht" />
             </fieldset>
-
             <input type="submit" defaultValue="Absenden" className="w-full my-12 daisy_btn bg-black text-white box-shadow" />
         </form>
     )
--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -11,19 +11,19 @@ export const Socials = ({styles}: {styles: string}) => {
     return (
         <nav className={styles}>
             <Link href="https://soundcloud.com/djraifu" rel="noreferrer noopener">
-                <Image src={soundcloud} alt="" />
+                <Image src={soundcloud} alt="soundcloud icon" />
             </Link>
             <Link href="https://www.instagram.com/dj.raifu/" rel="noreferrer noopener">
-                <Image src={instagram} alt="" />
+                <Image src={instagram} alt="instagram icon" />
             </Link>
             <Link href="https://www.tiktok.com/@dj.raifu?_t=8eGTzTctYiZ&_r=1" rel="noreferrer noopener">
-                <Image src={tiktok} alt="" />
+                <Image src={tiktok} alt="tiktok icon" />
             </Link>
             <Link href="https://www.facebook.com/raifu.absenger" rel="noreferrer noopener">
-                <Image src={facebook} alt="" />
+                <Image src={facebook} alt="facebook icon" />
             </Link>
             <Link href="https://twitter.com/djraifu" rel="noreferrer noopener">
-                <Image src={twitter} alt="" />
+                <Image src={twitter} alt="twitter icon" />
             </Link>
         </nav>
     )
