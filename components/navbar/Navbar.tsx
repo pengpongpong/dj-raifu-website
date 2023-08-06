e78d0a67f38b25302be6ed95e2251a95b1e3a1ca
Phuong T <t.m.phuong@hotmail.com>
2023-08-06 15:20:45 +0200

    fix navbar links

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -20,14 +20,14 @@ const Navbar = ({ navData }: { navData: NavbarProps }) => {
             {navData?.image ? <Image src={urlForImage(navData?.image).url()} className="w-[150px] md:w-[200px] lg:w-[250px] xl:w-[300px]" width={2560} height={1440} priority alt="Dj Raifu logo" /> : ""}
             <Socials styles={socialStyles} />
             <ul className="mt-2 gap-4 md:gap-6 md:mt-4 lg:gap-8 xl:gap-12 xl:mt-6 3xl:gap-16 tracking-wide md:tracking-wider lg:tracking-widest">
-                <li className="daisy_btn text-base bg-black text-white font-normal box-shadow hover:bg-white hover:text-black transition duration-300 ease-in-out">
-                    <Link href="/">Home</Link>
+                <li>
+                    <Link className="daisy_btn text-base bg-black text-white font-normal box-shadow hover:bg-white hover:text-black transition duration-300 ease-in-out" href="/">Home</Link>
                 </li>
-                <li className="daisy_btn text-base bg-black text-white font-normal box-shadow hover:bg-white hover:text-black transition duration-300 ease-in-out">
-                    <Link href="/ueber-mich">Über mich</Link>
+                <li>
+                    <Link className="daisy_btn text-base bg-black text-white font-normal box-shadow hover:bg-white hover:text-black transition duration-300 ease-in-out" href="/ueber-mich">Über mich</Link>
                 </li>
-                <li className="daisy_btn text-base bg-black text-white font-normal box-shadow hover:bg-white hover:text-black transition duration-300 ease-in-out">
-                    <Link href="/kontakt">Kontakt</Link>
+                <li>
+                    <Link className="daisy_btn text-base bg-black text-white font-normal box-shadow hover:bg-white hover:text-black transition duration-300 ease-in-out" href="/kontakt">Kontakt</Link>
                 </li>
             </ul>
         </nav>
